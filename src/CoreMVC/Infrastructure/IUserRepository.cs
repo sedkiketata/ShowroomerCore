c8eb898c66cee785e97ec8dea59d4a4351a180f2
sedkiketata <sedki.ketata@esprit.tn>
2017-04-03 23:15:28 +0100

    Create User Controller with its Repository

---
+++ src/CoreMVC/Infrastructure/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using CoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Infrastructure
+{
+    public interface IUserRepository
+    {
+        void Add(User item);
+        IEnumerable<User> GetAll();
+        User Find(long key);
+        void Remove(long key);
+        void Update(User item);
+    }
+}
--- src/CoreMVC/Startup.cs
+++ src/CoreMVC/Startup.cs
@@ -108,6 +108,7 @@ namespace CoreMVC
 
             // Register Application services
             services.AddSingleton<IProductRepository, ProductRepository>();
+            services.AddSingleton<IUserRepository, UserRepository>();
             
         }
 
